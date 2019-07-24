42fdb981b2e29ccfaace9def4c66068605e34572
Leonardo Ramos <leoramos@everis.com>
2019-07-24 16:52:39 -0300

    Calculando melhor volta de cada piloto, melhor volta da corrida e  velocidade média do piloto

--- src/main/java/gympass/kart/KartApplication.java
+++ src/main/java/gympass/kart/KartApplication.java
@@ -1,7 +1,10 @@
 package gympass.kart;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import gympass.kart.leitor.Leitor;
 import gympass.kart.tratamentos.CalculaRanking;
@@ -9,6 +12,7 @@ import gympass.kart.tratamentos.Tratamento;
 import gympass.kart.vo.Linha;
 import gympass.kart.vo.Piloto;
 import gympass.kart.vo.Ranking;
+import gympass.kart.vo.Volta;
 
 /**
  * 
@@ -19,15 +23,39 @@ import gympass.kart.vo.Ranking;
  */
 public class KartApplication {
 	
+	static CalculaRanking calculaRanking = new CalculaRanking();
+	
 	public static void main(String[] args) throws IOException {
 		
 		List<Linha> lista = new Leitor().ler();
 		
 		List<Piloto> pilotos = new Tratamento().retornaPilotos(lista);
 		
-		List<Ranking> rankings = new CalculaRanking().getRanking(pilotos);
-		
+		List<Ranking> rankings = calculaRanking.getRanking(pilotos);
+		System.out.println("1)");
+		System.out.println("Posição Chegada, Código Piloto, Nome Piloto, Qtde Voltas Completadas e Tempo Total de Prova:");
 		rankings.forEach(System.out::println);
+		System.out.println("\n===========================================================================================\n");
+		
+		List<Pair<String, Integer>> melhorVoltaCadaPiloto = calculaRanking.melhorVoltaCadaPiloto(pilotos);
+		System.out.println("2)");
+		System.out.println("Melhor volta dos pilotos:");
+		melhorVoltaCadaPiloto.forEach(System.out::println);
+		System.out.println("\n===========================================================================================\n");
+		
+		Pair<String, Volta> melhorVoltaCorrida = calculaRanking.melhorVoltaCorrida(pilotos);
+		System.out.println("3)");
+		System.out.println("Melhor volta da corrida:");
+		System.out.println(melhorVoltaCorrida);
+		System.out.println("\n===========================================================================================\n");
+		
+		List<Pair<String, BigDecimal>> velocidadeMediaPiloto = calculaRanking.velocidadeMediaPiloto(pilotos);
+		System.out.println("4)");
+		System.out.println("Velocidade média por piloto:");
+		velocidadeMediaPiloto.forEach(System.out::println);
+		System.out.println("\n===========================================================================================\n");
+		
+		
 	}
 	
 }
--- src/main/java/gympass/kart/tratamentos/CalculaRanking.java
+++ src/main/java/gympass/kart/tratamentos/CalculaRanking.java
@@ -1,11 +1,16 @@
 package gympass.kart.tratamentos;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import gympass.kart.vo.Piloto;
 import gympass.kart.vo.Ranking;
@@ -25,21 +30,22 @@ public class CalculaRanking {
 		List<Ranking> rankings = new ArrayList<>();
 
 		pilotos.forEach(piloto -> {
-			
+
 			Duration duracaoProva = piloto.getVoltas().stream().map(Volta::getTempoVolta).reduce(Duration::plus).get();
 
-			Ranking ranking = new Ranking(null, piloto.getId(), piloto.getNome(), piloto.getVoltas().size(), duracaoProva);
-			
+			Ranking ranking = new Ranking(null, piloto.getId(), piloto.getNome(), piloto.getVoltas().size(),
+					duracaoProva);
+
 			String duracao = formataDuracao(duracaoProva.toMillis());
-			
+
 			ranking.setDuracao(duracao);
-			
+
 			rankings.add(ranking);
 
 		});
-		
+
 		Collections.sort(rankings, Comparator.comparing(Ranking::getTempoTotal));
-		
+
 		setaColocacoes(rankings);
 
 		return rankings;
@@ -48,7 +54,63 @@ public class CalculaRanking {
 
 	/**
 	 * 
-	 * A lista já vem ordenada por menor tempo, então o primeiro é o primeiro da lista e assim por diante.
+	 * Calcula a melhor volta de cada piloto: isto é: aquela na qual o piloto
+	 * concluiu em menor tempo.
+	 * 
+	 * @param pilotos
+	 * @return
+	 */
+	public List<Pair<String, Integer>> melhorVoltaCadaPiloto(List<Piloto> pilotos) {
+
+		return pilotos.stream().map(piloto -> {
+
+			Collections.sort(piloto.getVoltas(), Comparator.comparing(Volta::getTempoVolta));
+
+			Volta melhorVolta = piloto.getVoltas().get(0);
+
+			return Pair.of(concatenaPilotoComId(piloto), melhorVolta.getIdVolta());
+
+		}).collect(Collectors.toList());
+
+	}
+
+	/**
+	 * 
+	 * Calcula a melhor volta da corrida.
+	 * 
+	 * @param pilotos
+	 * @return
+	 */
+	public Pair<String,Volta> melhorVoltaCorrida(List<Piloto> pilotos) {
+
+		List<Pair<String, Volta>> lista = pilotos.stream().map(piloto -> {
+			return Pair.of(concatenaPilotoComId(piloto),
+					piloto.getVoltas().stream().min(Comparator.comparing(Volta::getVelocidadeMediaVolta)).get());
+		}).collect(Collectors.toList());
+		
+		Collections.sort(lista, Comparator.comparing(pair -> pair.getRight().getVelocidadeMediaVolta()));
+		Collections.reverse(lista);
+
+		return lista.get(0);
+	}
+
+	public List<Pair<String, BigDecimal>> velocidadeMediaPiloto(List<Piloto> pilotos) {
+
+		return pilotos.stream().map(piloto -> {
+
+			BigDecimal mediaVelocidade = piloto.getVoltas().stream().map(Volta::getVelocidadeMediaVolta)
+					.reduce(BigDecimal::add).get()
+					.divide(BigDecimal.valueOf(piloto.getVoltas().size()), 3, RoundingMode.HALF_EVEN);
+
+			return Pair.of(concatenaPilotoComId(piloto), mediaVelocidade);
+		}).collect(Collectors.toList());
+
+	}
+
+	/**
+	 * 
+	 * A lista já vem ordenada por menor tempo, então o primeiro é o primeiro da
+	 * lista e assim por diante.
 	 * 
 	 * @param rankings
 	 */
@@ -59,25 +121,29 @@ public class CalculaRanking {
 		}
 	}
 
-	 public String formataDuracao(long millis) {
+	private String formataDuracao(long millis) {
 
-	        long days = TimeUnit.MILLISECONDS.toDays(millis);
-	        millis -= TimeUnit.DAYS.toMillis(days);
-	        long hours = TimeUnit.MILLISECONDS.toHours(millis);
-	        millis -= TimeUnit.HOURS.toMillis(hours);
-	        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
-	        millis -= TimeUnit.MINUTES.toMillis(minutes);
-	        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
-	        millis -= TimeUnit.SECONDS.toMillis(seconds);
+		long days = TimeUnit.MILLISECONDS.toDays(millis);
+		millis -= TimeUnit.DAYS.toMillis(days);
+		long hours = TimeUnit.MILLISECONDS.toHours(millis);
+		millis -= TimeUnit.HOURS.toMillis(hours);
+		long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+		millis -= TimeUnit.MINUTES.toMillis(minutes);
+		long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+		millis -= TimeUnit.SECONDS.toMillis(seconds);
 
-	        StringBuilder sb = new StringBuilder(64);
-	        sb.append(minutes);
-	        sb.append("min:");
-	        sb.append(seconds);
-	        sb.append("s:");
-	        sb.append(millis);
+		StringBuilder sb = new StringBuilder(64);
+		sb.append(minutes);
+		sb.append("min:");
+		sb.append(seconds);
+		sb.append("s:");
+		sb.append(millis);
 
-	        return(sb.toString());
-	    }
-	
+		return (sb.toString());
+	}
+
+	private String concatenaPilotoComId(Piloto piloto) {
+		return piloto.getId() + " - " + piloto.getNome();
+	}
+
 }
